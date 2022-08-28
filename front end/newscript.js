7fe9246b85b8bcea439267fbb47eea877bc9ae1e
Leo <leo@Leos-MacBook-Pro.local>
2022-08-29 00:09:55 +0300

    Clean up files

--- front end/newscript.js
+++
@@ -1,9 +0,0 @@
- function editReview(){
-    const textarea = document.getElementById('userreview');
-    let badwords = /cunt|bitch|shit|dick|asshole|doc/gi;
-    let userreview1 = textarea.value;
-    let userreview2 = userreview1.replace(badwords, '######');
-    document.getElementById('userreview').value = userreview2;
-}
-document.getElementById('submittext').addEventListener('click', editReview)    
-    
